140620a734591e508aee309b4c2e9a69faf8024c
miniwestxyz <hakennicklas@gmail.com>
2023-12-17 21:17:29 +0100

    New Design and Code

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-echo "Automated install for Docker CE"
-echo "Please enter your User Password"
-sudo -i
+YELLOW='\033[1;33m'
+Magenta='\033[0;35m'
+NC='\033[0m' 
+
+clear
+
+echo -e "${Magenta}Docker CE Install Script :)${NC}"
+echo -e "${Magenta}Profession natürlich :)${NC}"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-
-
+echo -e "${YELLOW}Installation fertig :)${NC}"
